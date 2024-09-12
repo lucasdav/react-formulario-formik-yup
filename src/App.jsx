b966177f60872471b64635ad023581975af8648e
lucasdav <lucassouzadavanso@gmail.com>
2024-09-12 16:58:42 -0300

    init proj

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,32 @@ import Input from './components/Input'
 import ErrorText from './components/ErrorText'
 import Button from './components/Button'
 import Form from './components/Form'
-import { Formik } from 'formik'
+import { ErrorMessage, Formik } from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
+  password: Yup.string().min(8, 'Senha deve ter no mínimo 8 caracteres').required('Senha é obrigatória'),
+  confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Senhas devem ser iguais').required('Confirmação de senha é obrigatória')
+})
 
 function App() {
 
-  const createUser = (values) => {
+  const createUser = (values, { resetForm }) => {
     console.log('Formulário submetido: ', values)
+
+    const user = values
+
+    axios.post('http://localhost:3000/users', user)
+      .then(() => {
+        alert('Usuário cadastrado com sucesso!')
+        resetForm()
+      })
+      .catch(error => {
+        console.error('Erro ao cadastrar usuário: ', error)
+      })
   }
 
   const initialValues = {
@@ -29,8 +49,10 @@ function App() {
         <Heading>
           Cadastro
         </Heading>
-        <Formik initialValues={initialValues} 
-        onSubmit={{ createUser }}
+        <Formik 
+          initialValues={initialValues} 
+          onSubmit={{ createUser }}
+          validationSchema={validationSchema}
         >
         <Form>
           <Fieldset>
@@ -41,9 +63,7 @@ function App() {
               name="name"
               id="name"
             />
-            {/* <ErrorText>
-              Nome é obrigatório
-            </ErrorText> */}
+            <ErrorMessage name="name" component={ErrorText}/>
           </Fieldset>
           <Fieldset>
             <Label>
@@ -54,6 +74,7 @@ function App() {
               type="email"
               id="email"
             />
+            <ErrorMessage email="email" component={ErrorText}/>
           </Fieldset>
           <Fieldset>
             <Label>
@@ -64,6 +85,7 @@ function App() {
               type="password"
               id="password"
             />
+            <ErrorMessage password="password" component={ErrorText}/>
           </Fieldset>
           <Fieldset>
             <Label>
@@ -74,6 +96,7 @@ function App() {
               type="password"
               id="confirmPassword"
             />
+            <ErrorMessage confirmPassword="confirmPassword" component={ErrorText}/>
           </Fieldset>
           <Button type='submit'>
             Enviar
